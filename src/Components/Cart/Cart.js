3aa3d8c1f13bf88d37f7dca6791e612b3ef94d68
MD Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-10-14 00:37:28 +0600

    cart total price showing for just products values

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
+    let total = 0;
+    for (const product of cart) {
+        total += product.price;
+    }
     return (
         <div>
             <h2>Order Summary</h2>
             <h4>Items Ordered: {cart.length}</h4>
+            <p>Total: {total}</p>
         </div>
     );
 };
