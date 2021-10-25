b790180c2c2a79175fccb4228aef22cf1431f4e4
MD Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-10-25 22:27:54 +0600

    cart component added in review component

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import useProducts from "../../hooks/useProducts";
 import useCart from "../../hooks/useCart";
+import Cart from "../Cart/Cart";
 
 const Review = () => {
     const [products] = useProducts();
     const [cart] = useCart(products);
     return (
-        <div>
-            <h1>This is review page: {products.length}</h1>
-            <h3>Cart: {cart.length}</h3>
+        <div className="shop-container">
+            <div className="product-container">
+                <h1>This is review page: {products.length}</h1>
+                <h3>Cart: {cart.length}</h3>
+            </div>
+
+            <div className="cart-container">
+                <Cart cart={cart}/>
+            </div>
+
         </div>
     );
 };
